0bc57055369623bcfd94519d26502beec51df95d
Kaj Siebert <kaj@k-si.com>
2021-09-07 17:10:17 +0100

    Add 'one_of' conditional

--- rtofdata/fake/faker.py
+++ rtofdata/fake/faker.py
@@ -1,3 +1,5 @@
+import re
+
 import yaml
 from faker import Faker
 from tqdm import trange
@@ -8,6 +10,8 @@ from rtofdata.fake.generators import get_date_or_delta
 from rtofdata.specification.parser import parse_specification
 
 faker = Faker()
+
+ptn_conditional = re.compile(r'(\w+)\((.*)\)')
 
 
 def generate_records(datastore, spec, context, record_name, config, progress=False):
@@ -40,35 +44,73 @@ def generate_records(datastore, spec, context, record_name, config, progress=Fal
             generate_records(datastore, spec, record_context, sub_record_name, sub_config)
 
 
+def generate_field_value(faker, context, field, field_config, record):
+    validators = [v['description'] for v in field.validation]
+    probability = 1.0 if "required(True)" in validators else 0.5
+    probability = field_config.get("probability", probability)
+
+    if faker.random.random() > probability:
+        return None
+
+    if field.foreign_keys:
+        gen = lambda *args, **kwargs: context['parent_id']
+    elif "method" in field_config:
+        gen = getattr(generators, field_config['method'])
+    else:
+        gen = getattr(generators, field.type.id)
+
+    args = {}
+    if "args" in field_config:
+        args = field_config.get('args', {})
+
+    return gen(faker, context, field=field, record=record, **args)
+
+
 def generate_record(spec, context, record_name, config, faker, ix):
     record_spec = spec.record_by_id(record_name)
     record = {}
 
+    fields_to_generate = {f.id: f for f in record_spec.fields}
+
     id = []
-    for f in record_spec.fields:
-        field_config = config.get("fields", {}).get(f.id, {})
-
-        validators = [v['description'] for v in f.validation]
-        probability = 1.0 if "required(True)" in validators else 0.5
-        probability = field_config.get("probability", probability)
-
-        if faker.random.random() > probability:
-            continue
-
-        if f.foreign_keys:
-            gen = lambda *args, **kwargs: context['parent_id']
-        elif "method" in field_config:
-            gen = getattr(generators, field_config['method'])
+    next_field = None
+    while len(fields_to_generate) > 0:
+        if next_field:
+            field_id = next_field
+            next_field = None
         else:
-            gen = getattr(generators, f.type.id)
+            field_id = next(iter(fields_to_generate))
+        field = fields_to_generate[field_id]
+        field_config = config.get("fields", {}).get(field.id, {})
+        field_config = {k: v for k, v in field_config.items()}
 
-        args = {}
-        if "args" in field_config:
-            args = field_config.get('args', {})
+        required_if = field_config.get("required_if")
+        if required_if:
+            match = ptn_conditional.match(required_if)
+            rule_name = match.group(1)
+            rule_value = match.group(2)
+            if rule_name == "one_of":
+                referenced_field = rule_value.split(",", 1)[0].strip()
+                if referenced_field not in record:
+                    next_field = referenced_field
+                    continue
+                value = record[referenced_field]
+                must_be = rule_value.split(",", 1)[1].strip()
+                must_be = yaml.safe_load(must_be)
+                if isinstance(must_be, list):
+                    if value in must_be:
+                        field_config['probability'] = 1
+                else:
+                    if value == must_be:
+                        field_config['probability'] = 1
+            else:
+                raise ValueError(f"Unknown rule: {rule_name}")
 
-        record[f.id] = gen(faker, context, field=f, record=record, **args)
-        if f.primary_key:
-            id.append(record[f.id])
+        record[field_id] = generate_field_value(faker, context, field, field_config, record)
+        if field.primary_key:
+            id.append(record[field_id])
+
+        del fields_to_generate[field_id]
 
     return id[0] if len(id) < 2 else tuple(id), record
 
