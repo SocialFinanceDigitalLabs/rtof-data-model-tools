b9af0e4202f82130035d7d7ab032f4153842d733
JamesHearn2 <james.hearn@socialfinance.org.uk>
2021-09-14 13:02:45 +0100

    Update to parser - now creates table, unquie on unique_id

--- rtofdata/parser.py
+++ rtofdata/parser.py
@@ -1,33 +1,61 @@
 #!/usr/bin/env python
 
 from dataclasses import field, fields
+import os
 from os import name
-from re import X
+from re import X, match
+from typing import ChainMap
+from typing_extensions import Concatenate
 from rtofdata.specification.data import Record
 import tablib
 from collections import namedtuple
 FieldRecord = namedtuple("FieldRecord", ["field", "record", "flow"])
 from rtofdata.specification.parser import parse_specification, validate_specification
+from collections import defaultdict
+from functools import reduce
+from itertools import chain, groupby
 
 from rtofdata.config import output_dir
 spec = parse_specification()
-
-data = output_dir / 'samples/sample_record_person.csv'
-
-
-with open(data, 'r') as fh:
-    data_tab = tablib.Dataset().load(fh)
-
-headers = data_tab.headers
-
 field_ids = [f.field.id for f in spec.fields if not f.field.foreign_keys]
 
 
-fields_included = [x for x in field_ids if x in headers]
-fields_not_included = [x for x in field_ids if x not in  headers]
+data = [output_dir / 'samples/sample_record_person.csv', output_dir / 'samples/sample_record_baseline.csv']
 
-x = data_tab.dict
-(x[:10])
+folderpath = output_dir / 'samples'
 
-print(x[:1])
-print(f"Fields not included in this submission {fields_not_included}")
+filepaths = [os.path.join(folderpath, name) for name in os.listdir(folderpath)] 
+
+all_files = []
+for path in data:
+    with open(path, 'r') as fh:
+        file = tablib.Dataset().load(fh)
+        all_files.append(file)
+
+headers = []
+for file in all_files:
+    headers_i = (file.headers)
+    headers += headers_i
+
+all_data = []
+for file in all_files:    
+    data_i = file.dict
+    all_data += data_i
+
+combine_on_id = map(lambda dict_tuple: dict(ChainMap(*dict_tuple[1])), 
+    groupby(sorted(all_data, key = lambda sub_dict: sub_dict["unique_id"]), 
+    key= lambda sub_dict: sub_dict["unique_id"]))
+
+unique_id_combine = list(combine_on_id)
+print(unique_id_combine[:2])
+
+
+
+# for file in all_files:
+  #  fields_included = [x for x in field_ids if x in headers]
+
+# for file in all_files:
+  #  fields_not_included = [x for x in field_ids if x not in  headers]
+
+#(f"Fields included in this submission: {fields_included}")
+#(f"Fields not included in this submission {fields_not_included}")
