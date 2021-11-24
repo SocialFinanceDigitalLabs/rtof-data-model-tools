902aecf22517220c5369c2ed2e1b596c87beee80
JamesHearn2 <james.hearn@socialfinance.org.uk>
2021-11-24 16:57:40 +0000

    update to validation

--- rtofdata/validation/validators/__init__.py
+++ rtofdata/validation/validators/__init__.py
@@ -63,10 +63,13 @@ def dimension(context, field_value, enabled):
 
 
 def count_min(context, field_value, enabled):
-    if enabled and len(field_value) < 3:
-        raise ValidationException ("Must have at least 3 values")
-    else :
-        pass
+    if field_value is None:
+            pass
+        else:
+            if enabled and len(field_value) < 3:
+                raise ValidationException ("Must have at least 3 values")
+            else :
+                pass
 
 
 def national_insurance_number(context, field_value, enabled):
