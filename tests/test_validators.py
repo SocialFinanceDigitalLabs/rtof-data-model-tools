c515b8e8cd8eda6ca2106e1d4d08d26a42eea230
Kaj Siebert <kaj@k-si.com>
2022-01-11 10:07:01 +0100

    Remove broken validator code

--- tests/test_validators.py
+++ tests/test_validators.py
@@ -1,6 +1,6 @@
 from rtofdata.validation import get_validator
 
 
-def test_get_validator(self):
+def test_get_validator():
     validator = get_validator('required')
     assert validator is not None
